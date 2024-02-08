247c54963f2797393e11317c9e7d92426e3e9ac0
wate <pc.poisoning@gmail.com>
2024-02-09 04:20:54 +0900

    update

--- functions/ical/[gcalid].ts
+++ functions/ical/[gcalid].ts
@@ -7,10 +7,10 @@ const iCalContent: PagesFunction = async (context) => {
     }
     return new Response(iCalData.body, {
       status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Max-Age": "86400",
-      },
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Max-Age": "86400",
+      // },
     });
   } catch (error) {
     return new Response(`${error.message}`, { status: 500 });
